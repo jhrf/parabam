cc9d7c6bbb1975f70adfeceed88e6abfb99c5006
Henry Farmery <jhrf2@cam.ac.uk>
2015-02-01 11:05:18 +0000

    This commit works on the server with complex men, but not on the mac with mouse

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ cmdclass['sdist'] = sdist
 
 setup(name='parabam',
       description='Parallel BAM File Analysis',
-      version='0.1.16dev',
+      version='0.1.17dev',
       author="JHR Farmery",
       license='BSD',
       author_email = 'jhrf2@cam.ac.uk',
