6ec958d7a9f39b3d16095376be39c8db7aaa3254
Henry Farmery <jhrf2@cam.ac.uk>
2015-02-24 14:01:12 +0000

    Pip didn't like the new version so we're moving on to 0.2.dev0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ cmdclass['sdist'] = sdist
 
 setup(name='parabam',
       description='Parallel BAM File Analysis',
-      version='0.1.dev26',
+      version='0.2.dev0',
       author="JHR Farmery",
       license='GPL',
       author_email = 'jhrf2@cam.ac.uk',
